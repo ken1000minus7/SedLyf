10a16df9a46208f9fc1e6a5ec626b86a9eeb1fd6
Manjot Singh Oberoi <yumbup@gmail.com>
2021-02-25 09:37:27 +0000

    many things

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+client=discord.Client()
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+@client.event 
+async def on_message(message):
+  if message.author==client.user:
+    return 
+  if message.content.startswith("$alone"):
+    await message.channel.send("I am not alone vro, loneliness is always with me")
+  if message.content.startswith("$studying"):
+    await message.channel.send('Yes, I am studying but "stu" is silent')
+  if message.content.startswith("$wanted"):
+    await message.channel.send('The only time I was wanted was when I was playing GTA')
+  if message.content.startswith("$life"):
+    await message.channel.send('Life is a party and I am a pinata')
+client.run(os.getenv("TOKEN"))
